89723b37faa3994e9ed12c4a041fa55ad0f84288
Orange <raffy.m.96@gmail.com>
2016-11-07 18:31:04 +0100

    Fix All

--- src/FigureGeometriche/Manager.java
+++ src/FigureGeometriche/Manager.java
@@ -5,33 +5,36 @@ import java.util.Scanner;
 public class Manager{
 	public Manager(Scanner sc) throws Exception{
 		figure = new ArrayList<FiguraGeometrica>();
-		FiguraGeometrica fg;
-		String tipo=sc.next();
-		
-		do{
-			if(!sc.hasNext()) 
-				fg=null;
-			else{
-				tipo=sc.next();
-				if(tipo.equals("Regolare"))
-					fg=FiguraRegolare.read(sc);
-				else if(tipo.equals("Rettangolo"))
-					fg=Rettangolo.read(sc);
-				else
-					fg=Cerchio.read(sc);
+		FiguraGeometrica fg=read(sc);
+		while(fg!=null){
 			figure.add(fg);
-			}
-		}while(fg!=null);
+			fg=read(sc);
+		}
+	}
+
+	private static FiguraGeometrica read(Scanner sc)  throws Exception{
+		FiguraGeometrica fg;
+		String tipo;
+		if(!sc.hasNext()) return null;
+		tipo=sc.next();
+		if(tipo.equals("Regolare"))
+			fg=FiguraRegolare.read(sc);
+		else if(tipo.equals("Rettangolo"))
+			fg=Rettangolo.read(sc);
+		else
+			fg=Cerchio.read(sc);
+
+		return fg;
 	}
 	
-	public double getAreaMax() {
+	public FiguraGeometrica getAreaMax() {
 		FiguraGeometrica max = figure.get(0);
 
 		for (int i = 1; i < figure.size(); i++)
 			if (max.calcolaArea() < figure.get(i).calcolaArea())
 				max = figure.get(i);
 		
-		return max.calcolaArea();
+		return max;
 	}
 	
 	public void print(PrintStream ps){
--- src/FigureGeometriche/Test.java
+++ src/FigureGeometriche/Test.java
@@ -6,7 +6,6 @@ public class Test {
 		Scanner sc = new Scanner(new File("figure"));
 		Manager manager = new Manager(sc);
 		
-		//manager.getAreaMax();
-		manager.print(System.out);
+		manager.getAreaMax().print(System.out);
 	}
 }
