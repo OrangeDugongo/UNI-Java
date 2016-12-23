dc6b10a3cb13f39bb4cb70d20d44557b850594cc
orange <raffy.m.96@gmail.com>
2016-12-23 12:49:18 +0100

    add canzone.java

---
+++ src/Radio/Canzone.java
@@ -0,0 +1,18 @@
+public class Canzone{
+
+    public String getTitolo(){
+        return titolo;
+    }
+
+    public String getAutore(){
+        return autore;
+    }
+
+    public String getAulbum(){
+        return album;
+    }
+
+    private String titolo;
+    private String autore;
+    private String album;
+}
