bac55784cef70db13c04b5267110e430504511db
orange <raffy.m.96@gmail.com>
2016-12-23 12:44:06 +0100

    rm centro ricerca

--- src/CentroRicerca/Constants.java
+++
@@ -1,6 +0,0 @@
-import java.text.SimpleDateFormat;
-
-public class Constants {
-	public static final String DATE_FORMAT = "dd-MM-yyyy";
-	public static final SimpleDateFormat S_DATE_FORMAT = new SimpleDateFormat(DATE_FORMAT);
-}
