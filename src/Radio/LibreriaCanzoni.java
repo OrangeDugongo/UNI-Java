ff2cab2baa64d1ee330934495daac2ea764ca710
orange <raffy.m.96@gmail.com>
2016-12-28 12:10:09 +0100

    add LibreriaCanzoni

--- src/Radio/LibreriaCanzoni.java
+++ src/Radio/LibreriaCanzoni.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class LibreriaCanzoni{
@@ -10,6 +11,46 @@ public class LibreriaCanzoni{
             lib.addLast(temp);
             temp=Canzone.read(sc);
         }
+    }
+
+    public LibreriaCanzoni(LinkedList<Canzone> lib){
+        this.lib=lib;
+    }
+
+    public LibreriaCanzoni filtroAutore(String autore){
+        LinkedList<Canzone> newLib = new LinkedList<Canzone>();
+        ListIterator<Canzone> iter = lib.listIterator();
+        while(iter.hasNext()){
+            Canzone temp=iter.next();
+            if(temp.getAutore().equals(autore))
+                newLib.addLast(temp);
+        }
+
+        return new LibreriaCanzoni(newLib);
+    }
+
+      public LibreriaCanzoni filtroTitolo(String titolo){
+        LinkedList<Canzone> newLib = new LinkedList<Canzone>();
+        ListIterator<Canzone> iter = lib.listIterator();
+        while(iter.hasNext()){
+            Canzone temp=iter.next();
+            if(temp.getAutore().equals(titolo))
+                newLib.addLast(temp);
+        }
+
+        return new LibreriaCanzoni(newLib);
+    }
+
+      public LibreriaCanzoni filtroAlbum(String album){
+        LinkedList<Canzone> newLib = new LinkedList<Canzone>();
+        ListIterator<Canzone> iter = lib.listIterator();
+        while(iter.hasNext()){
+            Canzone temp=iter.next();
+            if(temp.getAutore().equals(album))
+                newLib.addLast(temp);
+        }
+
+        return new LibreriaCanzoni(newLib);
     }
 
     private LinkedList<Canzone> lib;    
